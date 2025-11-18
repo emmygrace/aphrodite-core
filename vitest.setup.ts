442e401fbc6ac0235f42f142f8973cd65fba6ec9
emmy <emmy@aeliis.net>
2025-11-18 17:15:33 -0500

    1118 1715

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,6 @@
-// Jest setup file for DOM testing
+// Vitest setup file for DOM testing
 // Mock D3 if needed
-if (typeof global.d3 === 'undefined') {
+if (typeof (globalThis as any).d3 === 'undefined') {
   // D3 will be loaded via script tag in test.html or via import
   // For unit tests, we may need to mock it or load it differently
 }
